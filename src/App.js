2e5d15852b4fa87afd037b6a03c61a9e67eb01a7
Shan <shanmuganand.kanniappan@gmail.com>
2024-09-10 22:27:11 +0400

    netlify file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./components/Home/Home";,,,,,,,,,,,,,,,                                       
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
